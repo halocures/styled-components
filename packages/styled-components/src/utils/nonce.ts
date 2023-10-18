c3a7b5842b6297b5336f835a698fbeb790bb95cd
Amir Sharif <budgeneration@gmail.com>
2023-10-17 21:56:05 -0700

    Test

--- packages/styled-components/src/utils/nonce.ts
+++ packages/styled-components/src/utils/nonce.ts
@@ -1,5 +1,6 @@
 declare let __webpack_nonce__: string;
 
 export default function getNonce() {
+  console.log('getting nonce: %s', __webpack_nonce__);
   return typeof __webpack_nonce__ !== 'undefined' ? __webpack_nonce__ : null;
 }
